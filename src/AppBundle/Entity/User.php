3873f63ffa1e703b4b05dddea25dc2ca1a900860
Francisco Barahona S <fjbs17@gmail.com>
2018-04-14 21:22:25 -0500

    Integration of FOSUserBundle.

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,10 +1,10 @@
 <?php
-// src/AppBundle/Entity/User.php
 
 namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 /**
@@ -19,8 +19,40 @@ class User extends BaseUser {
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Assert\NotBlank(message="Por favor, indícanos tu nombre.", groups={"Registration", "Profile"})
+     */
+    protected $name;
+
+
     public function __construct() {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Set the value of name.
+     *
+     * @param string $name
+     * @return \AppBundle\Entity\User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
     }
 }
