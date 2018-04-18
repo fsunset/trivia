8f654869277660c1394b9ab4896f3e02d8cca327
Francisco Barahona S <fjbs17@gmail.com>
2018-04-18 14:33:51 -0500

    Ranking Users Modal set & Fix Issue with '5 Balones' after create account.

--- app/Resources/public/js/app.js
+++ app/Resources/public/js/app.js
@@ -1,6 +1,40 @@
 // Loads jQuery package from node_modules
-var $ = require('jquery');
+var $ = require("jquery");
 
 // JS is equivalent to the normal "bootstrap" package
 // no need to set this to a variable, just require it
-require('bootstrap-sass');
+require("bootstrap-sass");
+
+
+$(document).ready(function() {
+	let $rankingModalBtn = $("#rankingModalBtn"),
+		$rankingList = $("#rankingList");
+
+	$rankingModalBtn.on("click", function(e){
+		e.preventDefault();
+
+		let $this = $(this),
+			$url = $this.attr("data-url"),
+			$htmlcontent = '';
+
+		$.ajax({
+			url: $url,
+			method: "post",
+			data: {},
+			type: "json",
+			beforeSend: () => {
+				$rankingList.html('Cargando...');
+			},
+			success: function(users) {
+				$.each(JSON.parse(users), (index, user) => {
+					$htmlcontent += '<li>' + user.name + ' - <span>' + user.score + '</span></li>';
+				});
+
+				$rankingList.html($htmlcontent);
+			},
+			error: function(error) {
+				console.error("An unhandled error occurred in ajax success callback: " + error);
+			}
+		});
+	});
+});
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -16,6 +17,30 @@ class DefaultController extends Controller
         $user = $this->getUser();
 
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/ranking", name="ranking")
+     */
+    public function rankingAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            $users = $this->getDoctrine()->getRepository('AppBundle:User')->findBy(array(), array('score' => 'DESC', 'id' => 'DESC'));
+            $usersArray = [];
+
+            foreach ($users as $user) {
+                array_push($usersArray, array(
+                    'name' => $user->getName() . ' ' . $user->getLastName(),
+                    'score' => $user->getScore()
+                ));
+            }
+
+            return new Response(
+                json_encode($usersArray)
+            , 200);
+        }
+
+        throw new Exception("Error; la petición debe ser XmlHttp", 1);
     }
 
     /**
